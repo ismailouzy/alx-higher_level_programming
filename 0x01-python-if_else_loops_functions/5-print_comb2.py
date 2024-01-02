2e7ee3f10ae9b9688ddf81f154e7ee266cdfbd6b
ismailouzy <ismailouzal@gmail.com>
2024-01-02 14:00:10 -0800

    done it

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range (0, 10):
-        if j != 9:
-            print("{}{}".format(i, j), end=", ")
-        else:
-            print("{}{}".format(i, j))
+for delta in range(0, 100):
+    i = delta // 10
+    j = delta % 10
+    if delta != 99:
+        print("{}{}".format(i, j), end=", ")
+    else:
+        print("{}{}".format(i, j))

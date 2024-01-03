9eecb84ad7db428491ee9eab8291c55da32cdb67
ismailouzy <ismailouzal@gmail.com>
2024-01-02 16:08:42 -0800

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,6 +7,6 @@ def fizzbuzz():
             elif i % 5 == 0:
                 print("Buzz", end=" ")
             else:
-                print(i, end=" ") 
+                print(i, end=" ")
         elif i % 15 == 0:
             print("FizzBuzz", end=" ")

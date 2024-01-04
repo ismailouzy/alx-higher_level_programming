0d87111c4eb8b490aa0c534fb093ba57db3b7f5a
ismailouzy <ismailouzal@gmail.com>
2024-01-04 06:18:05 -0800

    fixed calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import calculator_1 as cal
-from sys import argv
+from sys import argv, exit
 if __name__ == "__main__":
     lent = int(len(argv) - 1)
-    if lent > 3:
+    if lent != 3:
             print("Usage: ./100-my_calculator.py <a> <operator> <b>") 
-            exit("1")
+            exit(1)
     else:
         for i in range(1, lent + 1):
             a = int(argv[1])
@@ -20,6 +20,6 @@ if __name__ == "__main__":
                 result = cal.div(a, b)
             else:
                 print("Unknown operator. Available operators: +, -, * and /") 
-                exit("1") 
+                exit(1) 
             
     print(result)

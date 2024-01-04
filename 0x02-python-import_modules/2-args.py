4c20431a911a8acee6f05e238253fc6c1c148695
ismailouzy <ismailouzal@gmail.com>
2024-01-04 03:02:12 -0800

    done

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    lent = len(argv) - 1
+    if lent == 0:
+        print(f"{lent} arguments.")
+    else:
+        print(f"{lent} arguments:")
+    for i in range(1, lent + 1):
+        print(f"{i}: {argv[i]}")
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    lent = len(argv) - 1
+    total = 0
+    for i in range(1, lent + 1):
+        total += int(argv[i])
+
+    print(total)
+

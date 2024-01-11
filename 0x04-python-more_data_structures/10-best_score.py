3b3c06064b98137db229896b870702eeee54f5a2
ismailouzy <ismailouzal@gmail.com>
2024-01-10 16:31:28 -0800

    dd

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return (max(a_dictionary))
-    return None
+        max = 0
+        for i in a_dictionary:
+            if a_dictionary[i] > max:
+                max = a_dictionary[i]
+        return max
+    else:
+        return None

da15db0233c076b709339fa9cfca50e481b5e7cf
ismailouzy <ismailouzal@gmail.com>
2024-01-10 18:00:55 -0800

    ge

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    toscore = 0
+    toweight = 0
+
+    for i, j in my_list:
+        toscore += i * j
+        toweight += j
+
+    avg = toscore / toweight
+    return avg

8d0cc57d5d99c999943a6fbb861e3f589335d1c4
ismailouzy <ismailouzal@gmail.com>
2024-01-23 10:30:08 -0800

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" class square """
+
+
+class Square:
+    """ class of a square"""
+
+    def __init__(self, size):
+        """ new square.
+        Args:
+            size: the size of square"""
+        self.__size = size

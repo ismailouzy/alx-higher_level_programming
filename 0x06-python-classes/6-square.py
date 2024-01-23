75dde422a3efbc2527c41afee42d72af8b7682b5
ismailouzy <ismailouzal@gmail.com>
2024-01-23 15:26:05 -0800

    pls

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,10 +37,10 @@ class Square:
 
     def my_print(self):
         """my print function"""
-        for i in range(0, self.__position[1]):
+        for i in range(self.__position[1]):
             print("")
-        for i in range(0, self.__size):
-            for k in range(0, self.__position[0]):
+        for i in range(self.__size):
+            for k in range(self.__position[0]):
                 print(" ", end="")
             for j in range(self.__size):
                 print("#", end="")

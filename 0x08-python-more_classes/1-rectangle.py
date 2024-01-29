aed112f3c1a63603b860fc4eed3a351118bd1e66
ismailouzy <ismailouzal@gmail.com>
2024-01-29 10:55:03 -0800

    got confused between property and setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,6 +17,10 @@ class Rectangle:
         self.__height = height
 
     @property
+    def width(self):
+        return self.__width
+
+    @width.setter
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -25,6 +29,10 @@ class Rectangle:
         self.__width = value
 
     @property
+    def height(self):
+        return self.__height
+
+    @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")

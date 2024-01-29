ea06aeab0e401b819d48ecbd836d6d764f4a2592
ismailouzy <ismailouzal@gmail.com>
2024-01-29 15:32:20 -0800

    removed trailing whitespeace

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """a class that defines a rectangle"""
-    number_of_instances = 0        
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initializes a new rectangle

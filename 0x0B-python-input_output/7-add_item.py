7a8ef7473ddd461216d1e7d1afd3798782ca0513
ismailouzy <ismailouzal@gmail.com>
2024-02-06 15:15:54 -0800

    done

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""""input / output
+
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    e_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    e_list = []
+
+save_to_json_file(e_list, 'add_item.json')
+
+if __name__ == "__main__":
+    argsa = sys.argv[1:]
+    e_list.extend(argsa)

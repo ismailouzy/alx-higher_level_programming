ba857a46bd2cd31ec72d311b0e7113e9acb9c398
ismailouzy <ismailouzal@gmail.com>
2024-02-06 15:27:51 -0800

    done

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""input output
+
+"""
+
+
+def class_to_json(obj):
+    """a function that returns the dictionary
+    description with simple data structure
+        Args:
+            obj:
+    """
+    return obj.__dict__

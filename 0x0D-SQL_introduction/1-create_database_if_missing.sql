702ac90cbb6d8b13f26476c14216008518cc8b9e
ismailouzy <ismailouzal@gmail.com>
2024-03-12 15:31:49 -0700

    ddone

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- a script that creates a database called hbtn_0c_0
-CREATE IF NOT EXISTS `hbtn_0c_0`;
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- script that lists all records of the table
+SELECT * FROM second_table where name IS NOT NULL AND name != "" ORDER BY score DESC;

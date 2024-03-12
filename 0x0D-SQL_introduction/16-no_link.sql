0d458ad9bbd8978b7eb61c2f6ae2a11eb9bde817
ismailouzy <ismailouzal@gmail.com>
2024-03-12 16:40:19 -0700

    d

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table
-SELECT * FROM second_table AS number WHERE name != "" ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name != "" ORDER BY score DESC;

bdb5715dfdc7d5f56c34a61479e59eb54d3af8eb
ismailouzy <ismailouzal@gmail.com>
2024-04-08 15:11:17 -0700

    d

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@ const arg = process.argv[2];
 const noccur = parseInt(arg);
 
 if (!isNaN(noccur)) {
-	for (let i = 0; i < arg; i++) {
-		console.log("C is fun");
-	}
+  for (let i = 0; i < arg; i++) {
+    console.log('C is fun');
+  }
 } else {
-	console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 }

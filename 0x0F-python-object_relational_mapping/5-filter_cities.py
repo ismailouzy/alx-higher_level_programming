3fc6ef4b87ddf242e7d63931fc369f29a3a47ecc
ismailouzy <ismailouzal@gmail.com>
2024-04-14 18:16:11 -0700

    d

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,6 @@ if __name__ == '__main__':
                 "JOIN states ON cities.state_id = states.id "
                 "WHERE states.name = %s ORDER BY cities.id ASC", [sys.argv[4]])
     rows = cur.fetchall()
-    
     j = []
     for i in rows:
         j.append(i[1])

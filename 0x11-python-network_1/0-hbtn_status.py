6037b3907473b05f20dbf39bf34bea9b8bcd80de
ismailouzy <ismailouzal@gmail.com>
2024-04-26 16:06:14 -0700

    d

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,12 @@
 """What's my status?"""
 
 import urllib.request
+
 if __name__ == "__main__":
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        rap = response.read()
         print("Body response:")
-        print("\t- type:", type(response.read()))
-        print("\t- content:", response.read())
-        print("\t- utf8 content:", response.read().decode('utf-8'))
+        print("\t- type: {}".format(type(rap)))
+        print("\t- content: {}".format(rap))
+        print("\t- utf8 content: {}".format(rap.decode('utf-8')))
